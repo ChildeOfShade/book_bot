9441874be048c7c7bb6c10a6eafedbb95a83e936
TheLaneagen <devinjyork@gmail.com>
2025-06-06 18:59:22 -0500

    Forgot to add main.py changes

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
     
-def count_words(words):
-    num_words = words.split()
-    return len(num_words)
 
 
 def main():
