51412a6ed7f6e2d7c14a635ce412ee2cedef4767
TheLaneagen <devinjyork@gmail.com>
2025-06-06 18:51:19 -0500

    Added word count definition and print statement

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
+    
+def count_words(words):
+    num_words = words.split()
+    return len(num_words)
+
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    word_count = count_words(book_text)
+    print(f"{word_count} words found in the document!")
 
 if __name__ == "__main__":
     main()
