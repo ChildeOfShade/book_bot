fd0d07e0195e55dd8013c4432a5cba640ea77e33
TheLaneagen <devinjyork@gmail.com>
2025-06-06 19:16:16 -0500

    Formatted my output and cleaned it up.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, print_character_counts
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
+
+book_path = "books/frankenstein.txt"
     
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     char_count = count_characters(book_text)
-    print(f"{word_count} words found in the document!")
-    print(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/{book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    print_character_counts(char_count)
+    
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -10,3 +10,11 @@ def count_characters(text):
         else:
             char_count[char] = 1
     return char_count
+
+def print_character_counts(c_count):
+    sorted_chars = sorted(c_count.items(), key=lambda item: item[1], reverse=True)
+
+    for char, count in sorted_chars:
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
