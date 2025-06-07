34ee63ca93339e3a68391f8dffd49c9075d1e081
TheLaneagen <devinjyork@gmail.com>
2025-06-07 16:55:07 -0500

    Added system argument to command.

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from stats import count_words, count_characters, print_character_counts
+import sys
+
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
 
-book_path = "books/frankenstein.txt"
-    
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     char_count = count_characters(book_text)
+
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/{book_path}")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
     print_character_counts(char_count)
-    
 
 if __name__ == "__main__":
     main()
